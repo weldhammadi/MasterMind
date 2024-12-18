ef10e41e20a46058d415363877ea388014771055
hamza elmortada <hamza.el-mortada@edu.isga.ma>
2024-12-18 14:27:15 +0100

    implementaion of get_feedback

--- main.py
+++ main.py
@@ -12,11 +12,31 @@ def get_feedback(secret_code, guess):
     
     
     #initialize variables list
+    feedback = ['x'] * 4
+    guess_used = [False] * 4
+    secret_used = [False] * 4
+    
     
     #check for exact matches
-
+    for index in range(4):
+        if guess[index] == secret_code[index]:
+            feedback[index] = 'o'
+            guess_used[index] = True
+            secret_used[index] = True
+    
+    
     #check for wrong position matches        
-    print(" ")
+    for index_1 in range(4):
+        if feedback[index_1] == 'x':
+            for index_2 in range(4):
+                if not secret_used[index_2] and not guess_used[index_1]:
+                    if guess[index_1] == secret_code[index_2]:
+                        feedback[index_1] = '+'
+                        secret_used[index_2] = True
+                        break
+                    
+                    
+    return feedback
 
 def print_feedback(guess, feedback):
     #print the guess and its feedback as combination of o x and +
